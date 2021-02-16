3b9c5c949a84b302ffcbbb2d8759cea8e6b4d0dd
Sidhanth Kafley <skafley@uvm.edu>
2021-02-15 21:34:18 -0500

    Completed many Links

--- generate.py
+++ generate.py
@@ -4,13 +4,37 @@ pyrosim.Start_SDF("boxes.sdf")
 length = 1
 width = 1
 height = 1
-x=0
+i=0
 y=0
 z=0.5
-x2=1
-y2=0
-z2=1.5
-pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length, width, height])
-pyrosim.Send_Cube(name="Box2", pos=[x2,y2,z2] , size=[length, width, height])
+for x in range(6):
+    for a in range(6):
+        for j in range(10):
+            pyrosim.Send_Cube(name="Box", pos=[i,y,z] , size=[length, width, height])
+            z+=height/1.05
+            length*=0.9
+            width*=0.9
+            height*=0.9
+        length = 1
+        width = 1
+        height = 1
+        z=0.5
+        i+=1
+    length = 1
+    width = 1
+    height = 1
+    i=0
+    y+=1
+    
+    
+#for x in range(10):
+#    pyrosim.Send_Cube(name="Box", pos=[i,y,z] , size=[length, width, height])
+#    z+=height/1.05
+#    length*=0.9
+#    width*=0.9
+#    height*=0.9
 
+    
+    
+            
 pyrosim.End()
