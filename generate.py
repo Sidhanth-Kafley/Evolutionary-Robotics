910b755313477eedded8d0913be92ef9efb6e649
Sidhanth Kafley <skafley@uvm.edu>
2021-02-15 23:46:48 -0500

    Completed joints

--- generate.py
+++ generate.py
@@ -22,13 +22,12 @@ def Create_Robot():
     length = 1
     width = 1
     height = 1
-    i=0
-    y=0
-    z=0.5
     
-    pyrosim.Send_Cube(name="Torso", pos=[i,y,z] , size=[length, width, height])
-    pyrosim.Send_Joint( name = "Torso_Leg" , parent= "Torso" , child = "Leg" , type = "revolute", position = "0.5 0 1.0")
-    pyrosim.Send_Cube(name="Leg", pos=[0.5,0,0.5] , size=[length, width, height])
+    pyrosim.Send_Cube(name="BackLeg", pos=[0,0,0.5] , size=[length, width, height])
+    pyrosim.Send_Joint( name = "BackLeg_Torso" , parent= "BackLeg" , child = "Torso" , type = "revolute", position = "0.5 0 1.0")
+    pyrosim.Send_Cube(name="Torso", pos=[0.5,0,0.5] , size=[length, width, height])
+    pyrosim.Send_Joint( name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = "1.0 0 0.0")
+    pyrosim.Send_Cube(name="FrontLeg", pos=[0.5,0,-0.5] , size=[length, width, height])
     
     pyrosim.End()
 
