5cbd01946bd2da275e786419d02140f405f2f060
Sidhanth Kafley <skafley@uvm.edu>
2021-02-15 22:19:19 -0500

    Completed Defining a robot

--- generate.py
+++ generate.py
@@ -1,40 +1,53 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("boxes.sdf")
-length = 1
-width = 1
-height = 1
-i=0
-y=0
-z=0.5
-for x in range(6):
-    for a in range(6):
-        for j in range(10):
-            pyrosim.Send_Cube(name="Box", pos=[i,y,z] , size=[length, width, height])
-            z+=height/1.05
-            length*=0.9
-            width*=0.9
-            height*=0.9
-        length = 1
-        width = 1
-        height = 1
-        z=0.5
-        i+=1
+
+def Create_World():
+
+    pyrosim.Start_SDF("world.sdf")
+    length = 1
+    width = 1
+    height = 1
+    i=-3
+    y=3
+    z=0.5
+
+    pyrosim.Send_Cube(name="Box", pos=[i,y,z] , size=[length, width, height])
+    
+    pyrosim.End()
+    
+    
+def Create_Robot():
+
+    pyrosim.Start_URDF("body.urdf")
     length = 1
     width = 1
     height = 1
     i=0
-    y+=1
+    y=0
+    z=0.5
     
+    pyrosim.Send_Cube(name="Torso", pos=[i,y,z] , size=[length, width, height])
     
-#for x in range(10):
-#    pyrosim.Send_Cube(name="Box", pos=[i,y,z] , size=[length, width, height])
-#    z+=height/1.05
-#    length*=0.9
-#    width*=0.9
-#    height*=0.9
+    pyrosim.End()
 
-    
-    
-            
-pyrosim.End()
+
+Create_World()
+Create_Robot()
+#for x in range(6):
+#    for a in range(6):
+#        for j in range(10):
+#            pyrosim.Send_Cube(name="Box", pos=[i,y,z] , size=[length, width, height])
+#            z+=height/1.05
+#            length*=0.9
+#            width*=0.9
+#            height*=0.9
+#        length = 1
+#        width = 1
+#        height = 1
+#        z=0.5
+#        i+=1
+#    length = 1
+#    width = 1
+#    height = 1
+#    i=0
+#    y+=1
