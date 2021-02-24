9b37724e640d23bfcbbeb81b62d41c29316ae5b0
Sidhanth Kafley <skafley@uvm.edu>
2021-02-23 20:54:26 -0500

    Completed prettifying the visualization

--- analyze.py
+++ analyze.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot
 
 backLegSensorValues = np.load('data/backLegSensorValues.npy')
 frontLegSensorValues = np.load('data/frontLegSensorValues.npy')
-matplotlib.pyplot.plot(backLegSensorValues)
+matplotlib.pyplot.plot(backLegSensorValues, linewidth=3)
 matplotlib.pyplot.plot(frontLegSensorValues)
+matplotlib.pyplot.legend()
 matplotlib.pyplot.show()
