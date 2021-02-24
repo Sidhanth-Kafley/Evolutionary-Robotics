eaf041fc96a4c50b97bdeae01fbf84f6cd1341d8
Sidhanth Kafley <skafley@uvm.edu>
2021-02-23 20:39:15 -0500

    Completed multiple sensors

--- analyze.py
+++ analyze.py
@@ -2,5 +2,7 @@ import numpy as np
 import matplotlib.pyplot
 
 backLegSensorValues = np.load('data/backLegSensorValues.npy')
+frontLegSensorValues = np.load('data/frontLegSensorValues.npy')
 matplotlib.pyplot.plot(backLegSensorValues)
+matplotlib.pyplot.plot(frontLegSensorValues)
 matplotlib.pyplot.show()
