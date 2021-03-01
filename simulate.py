1d0dbf24b7ac7e8d62efa7a961305e39ec788414
Sidhanth Kafley <skafley@uvm.edu>
2021-03-01 14:19:39 -0500

    Made robot stand on its tip toes

--- simulate.py
+++ simulate.py
@@ -3,18 +3,18 @@ import pybullet_data
 import time
 import pyrosim.pyrosim as pyrosim
 import numpy as np
+import math
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
-planeId = p.loadURDF("body.urdf")
 robot = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
-backLegSensorValues = np.zeros(100)
-frontLegSensorValues = np.zeros(100)
+backLegSensorValues = np.zeros(1000)
+frontLegSensorValues = np.zeros(1000)
 pyrosim.Prepare_To_Simulate("body.urdf")
-for i in range(100):
+for i in range(1000):
     p.stepSimulation()
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
@@ -22,8 +22,14 @@ for i in range(100):
     bodyIndex = robot,
     jointName = "Torso_BackLeg",
     controlMode = p.POSITION_CONTROL,
-    targetPosition = ...,
-    maxForce = ...)
+    targetPosition = -(math.pi)/4.0,
+    maxForce = 500)
+    pyrosim.Set_Motor_For_Joint(
+    bodyIndex = robot,
+    jointName = "Torso_FrontLeg",
+    controlMode = p.POSITION_CONTROL,
+    targetPosition = +(math.pi)/4.0,
+    maxForce = 500)
     time.sleep(1/60)
 np.save('data/backLegSensorValues.npy', backLegSensorValues)
 np.save('data/frontLegSensorValues.npy', frontLegSensorValues)
