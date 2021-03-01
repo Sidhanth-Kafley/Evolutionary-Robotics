9415346e8f7a69c7b86cac712645a480a025496a
Sidhanth Kafley <skafley@uvm.edu>
2021-03-01 13:53:23 -0500

    Added a motor

--- simulate.py
+++ simulate.py
@@ -9,6 +9,7 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
 planeId = p.loadURDF("body.urdf")
+robot = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
 backLegSensorValues = np.zeros(100)
 frontLegSensorValues = np.zeros(100)
@@ -17,6 +18,12 @@ for i in range(100):
     p.stepSimulation()
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+    pyrosim.Set_Motor_For_Joint(
+    bodyIndex = robot,
+    jointName = "Torso_BackLeg",
+    controlMode = p.POSITION_CONTROL,
+    targetPosition = ...,
+    maxForce = ...)
     time.sleep(1/60)
 np.save('data/backLegSensorValues.npy', backLegSensorValues)
 np.save('data/frontLegSensorValues.npy', frontLegSensorValues)
