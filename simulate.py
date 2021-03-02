d99f575563a31719a4a5e847796bbd1f500ac5de
Sidhanth Kafley <skafley@uvm.edu>
2021-03-01 20:58:37 -0500

    Open-loop control of movement

--- simulate.py
+++ simulate.py
@@ -3,7 +3,6 @@ import pybullet_data
 import time
 import pyrosim.pyrosim as pyrosim
 import numpy as np
-import math
 import random
 
 physicsClient = p.connect(p.GUI)
@@ -15,25 +14,35 @@ p.loadSDF("world.sdf")
 backLegSensorValues = np.zeros(1000)
 frontLegSensorValues = np.zeros(1000)
 pyrosim.Prepare_To_Simulate("body.urdf")
+amplitude_backLeg = np.pi/4
+frequency_backLeg = 10
+phaseOffset_backLeg = 0
+n = np.linspace(-np.pi, np.pi, 1000)
+targetAngles_backLeg = np.sin(frequency_backLeg * n + phaseOffset_backLeg) * amplitude_backLeg
+amplitude_frontLeg = np.pi/4
+frequency_frontLeg = 40
+phaseOffset_frontLeg = np.pi
+targetAngles_frontLeg = np.sin(frequency_frontLeg * n + phaseOffset_frontLeg) * amplitude_frontLeg
+#np.save('data/targetAngles_backLeg.npy', targetAngles_backLeg)
+#np.save('data/targetAngles_frontLeg.npy', targetAngles_frontLeg)
+#exit();
 for i in range(1000):
     p.stepSimulation()
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
-    min = -(math.pi)/2.0
-    max = (math.pi)/2.0
     pyrosim.Set_Motor_For_Joint(
     bodyIndex = robot,
     jointName = "Torso_BackLeg",
     controlMode = p.POSITION_CONTROL,
-    targetPosition = min + (random.random() * (max - min)),
+    targetPosition = targetAngles_backLeg[i],
     maxForce = 30)
     pyrosim.Set_Motor_For_Joint(
     bodyIndex = robot,
     jointName = "Torso_FrontLeg",
     controlMode = p.POSITION_CONTROL,
-    targetPosition = min + (random.random() * (max - min)),
+    targetPosition = targetAngles_frontLeg[i],
     maxForce = 30)
-    time.sleep(1/1000)
+    time.sleep(1/800)
 np.save('data/backLegSensorValues.npy', backLegSensorValues)
 np.save('data/frontLegSensorValues.npy', frontLegSensorValues)
 p.disconnect()
