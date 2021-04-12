53b72d79be0eea21eab57a49569984f760e4b356
Sidhanth Kafley <skafley@uvm.edu>
2021-04-12 17:11:05 -0400

    Added left leg

--- solution.py
+++ solution.py
@@ -69,6 +69,9 @@ class SOLUTION():
         pyrosim.Send_Cube(name="FrontLeg", pos=[0,0.5,0] , size=[0.2, 1, 0.2])
         pyrosim.Send_Joint( name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = "0 -0.5 1", jointAxis = "1 0 0")
         pyrosim.Send_Cube(name="BackLeg", pos=[0,-0.5,0] , size=[0.2, 1, 0.2])
+        pyrosim.Send_Joint( name = "Torso_LeftLeg" , parent= "Torso" , child = "LeftLeg" , type = "revolute", position = "-0.5 0, 1", jointAxis = "0 1 0")
+        pyrosim.Send_Cube(name="LeftLeg", pos=[-0.5,0,0] , size=[1, 0.2, 0.2])
+
 
         pyrosim.End()
 
@@ -80,8 +83,10 @@ class SOLUTION():
         pyrosim.Send_Sensor_Neuron(name = 0 , linkName = "Torso")
         pyrosim.Send_Sensor_Neuron(name =1 , linkName = "BackLeg")
         pyrosim.Send_Sensor_Neuron(name = 2 , linkName = "FrontLeg")
-        pyrosim.Send_Motor_Neuron(name = 3 , jointName = "Torso_BackLeg")
-        pyrosim.Send_Motor_Neuron(name = 4 , jointName = "Torso_FrontLeg")
+        pyrosim.Send_Sensor_Neuron(name = 3 , linkName = "LeftLeg")
+        pyrosim.Send_Motor_Neuron(name = 4 , jointName = "Torso_BackLeg")
+        pyrosim.Send_Motor_Neuron(name = 5 , jointName = "Torso_FrontLeg")
+        pyrosim.Send_Motor_Neuron(name = 6 , jointName = "Torso_LeftLeg")
 
         for currentRow in range(len(self.weights)):
             for currentColumn in range(len(self.weights[0])):
