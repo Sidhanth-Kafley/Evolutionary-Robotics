889e22230809c819ff4182fe9c605f2bd503fb46
Sidhanth Kafley <skafley@uvm.edu>
2021-04-12 10:00:54 -0400

    Moved torso to the center of the simulation

--- solution.py
+++ solution.py
@@ -64,7 +64,7 @@ class SOLUTION():
         width = 1
         height = 1
 
-        pyrosim.Send_Cube(name="Torso", pos=[0,0,1.5] , size=[length, width, height])
+        pyrosim.Send_Cube(name="Torso", pos=[0,0,1] , size=[length, width, height])
         pyrosim.Send_Joint( name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = "0.5 0 1")
         pyrosim.Send_Cube(name="FrontLeg", pos=[0.5,0,-0.5] , size=[length, width, height])
         pyrosim.Send_Joint( name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = "-0.5 0 1")
